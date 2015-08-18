fa90ee1e3a8239b3d27dc9e9fc66b802330c390c
silvernao18 <silver.nao.18@gmail.com>
2015-08-18 03:29:09 +0000

    Install the devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-    
   # ページ固有のタイトル有無でタイトルの文字列を調整して返す。# コメント行
   def full_title(page_title)                          # ヘルパー（メソッド）の定義
     base_title = "Ruby on Rails Tutorial Sample App"  # 変数にデフォルトのタイトルを文字列として保持
@@ -7,6 +6,21 @@ module ApplicationHelper
       base_title                                      # デフォルトの状態で暗黙の返り値として使用
     else
       "#{base_title} | #{page_title}"                 # 文字列を結合させてタイトルを完成させたものを返す
+    end
+  end
+ 
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when :success
+        "alert-success"
+      when :error
+        "alert-danger"
+      when :alert
+        "alert-warning"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
     end
   end
 end

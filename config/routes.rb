99b7d6202e2aee8088be15ff347738f4abb34f00
silvernao18 <silver.nao.18@gmail.com>
2015-08-19 06:08:04 +0000

    Finish user index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 SampleApp::Application.routes.draw do
+  
   root  'static_pages#home'
   devise_for :users, :controllers => {
     :registrations => "registrations"
   }
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
